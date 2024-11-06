7df2a54ab89e0df06279924176b07660bab001a0
= <potlavani9194@gmail.com>
2024-11-06 21:52:46 +0530

    Media responsive and Login page is designed

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-const Navbar = () => {
+import {Link} from 'react-router-dom'
+const Navbar = ({setShowLogin}) => {
     const [menu,setMenu]=useState("home");
   return (
     <div className='navbar'>
       <img src={assets.logo} className='logo' alt=""/>
       <ul className='navbar-menu'>
-        <li onClick={()=> setMenu("home")} className={menu==="home"?"active":""}>Home</li>
-        <li onClick={()=> setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
-        <li onClick={()=> setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-App</li>
-        <li onClick={()=> setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</li>
+        <Link to='/' onClick={()=> setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
+        <a href="#explore-menu" onClick={()=> setMenu("menu")} className={menu==="menu"?"active":""}>Menu</a>
+        <a href="#app-download" onClick={()=> setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-App</a>
+        <a href="#footer" onClick={()=> setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</a>
       </ul>
     <div className='navbar-right'>
         <img src={assets.search_icon} alt="" />
@@ -18,7 +19,7 @@ const Navbar = () => {
             <img src={assets.basket_icon} />
             <div className='dot'></div>
         </div>
-        <button>Sign In</button>
+        <button onClick={()=>setShowLogin(true)}>Sign In</button>
     </div>
     </div>
   )
